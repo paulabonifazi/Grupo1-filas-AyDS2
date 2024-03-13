d335266c99c9daf31b078bb9b293b4f7bb2077fc
paulabonifazi <paula.bonifazi.aquino@gmail.com>
2024-03-12 21:23:31 -0300

    .

--- src/front/VistaOperador.java
+++ src/front/VistaOperador.java
@@ -1,7 +1,16 @@
 package front;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JSplitPane;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JSeparator;
+import javax.swing.JTextArea;
 
 public class VistaOperador extends JFrame implements IVistaOperador {
+	public VistaOperador() {
+	}
 
 }
