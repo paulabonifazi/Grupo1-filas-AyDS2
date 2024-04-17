32bd491844ee777279cef1076454741797ba7307
NicoAgHerrera <132090401+NicoAgHerrera@users.noreply.github.com>
2024-04-17 18:12:35 -0300

    Estructura Conexiones

--- src/back/Conexiones.java
+++ src/back/Conexiones.java
@@ -1,9 +1,31 @@
 package back;
 
-public class Conexiones {
-	private IConexion lol; //falta estructura de datos, pero es de elementos Iconexion
-	public Conexiones() {
-		// TODO Auto-generated constructor stub
-	}
+import java.util.Iterator;
+import java.util.concurrent.ConcurrentHashMap;
 
+public class Conexiones {
+	private ConcurrentHashMap<String, IConexion> arregloConexiones;
+
+	public Conexiones() {
+		this.arregloConexiones= new ConcurrentHashMap<String, IConexion>();
+	}
+	
+	public void agregarConexion(String id, IConexion conexion) {
+		arregloConexiones.put(id, conexion);
+	}
+	
+	// Obtener conexión
+    public IConexion obtenerConexion(String id) { //devuelve null si no existe
+        return arregloConexiones.get(id);
+    }
+    
+ // Obtener todas las conexiones
+    public Iterator<IConexion> obtenerTodasLasConexiones() {
+        return this.arregloConexiones.values().iterator();
+    }
+    
+    // Verificar si una conexión existe
+    public boolean existeConexion(String id) {
+        return arregloConexiones.containsKey(id);
+    }
 }
