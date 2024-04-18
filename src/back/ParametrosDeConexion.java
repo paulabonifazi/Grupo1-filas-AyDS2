24f84f1403d314a7e613d45e1380e0f9bc772473
NicoAgHerrera <132090401+NicoAgHerrera@users.noreply.github.com>
2024-04-17 21:28:51 -0300

    Termino de borrar

--- src/back/ParametrosDeConexion.java
+++ src/back/ParametrosDeConexion.java
@@ -11,25 +11,6 @@ public class ParametrosDeConexion {
 	public ParametrosDeConexion(String contraseña) {
 		this.contraseña=contraseña;
 	}
-	
-	public void agregarConexion(String id, IConexion conexion) {
-		arregloConexiones.put(id, conexion);
-	}
-	
-	// Obtener conexión
-    public IConexion obtenerConexion(String id) { //devuelve null si no existe Y SOLAMENTE SE PUEDE CONSULTAR (get) xq sino habria que sincronizarlo.
-        return arregloConexiones.get(id);
-    }
-    
- // Obtener todas las conexiones
-    public Iterator<IConexion> obtenerTodasLasConexiones() {
-        return this.arregloConexiones.values().iterator();
-    }
-    
-    // Verificar si una conexión existe
-    public boolean existeConexion(String id) {
-        return arregloConexiones.containsKey(id);
-    }
 
 	public int getPuertoLibre() {
 		return PuertoLibre;
